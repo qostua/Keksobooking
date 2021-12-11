066e3b49a019b8a6c7df0d99cfa280a21a793e49
qostua <qostua@gmail.com>
2021-12-11 17:51:56 +0300

    Добавил функцию получения случайного числа с плавающей точкой в диапазоне

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+function getRandomInt(val1, val2) {
+  let max = (val2 >= val1) ? val2 : val1;
+  let min = (val2 >= val1) ? val1 : val2;
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomInt(0, 1);
 
 function getRandomFloorInt(val1, val2, lenght) {
   let max = (val2 >= val1) ? val2 : val1;
@@ -8,4 +18,4 @@ function getRandomFloorInt(val1, val2, lenght) {
   return (Math.floor(Math.random() * (max - min + 1)) + min) / 10 ** lenght;
 }
 
-console.log(getRandomFloorInt(1.1, 2.1, 10));
+getRandomFloorInt(1.1, 2.1, 10);
