5b0bed7fa5372f3a875d3b54658c3914fd0ab103
qostua <qostua@gmail.com>
2021-12-12 11:33:43 +0300

    Взял функции генерации чисел из обновлений Кекса

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-function getRandomInt(val1, val2) {
-  let max = (val2 >= val1) ? val2 : val1;
-  let min = (val2 >= val1) ? val1 : val2;
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomPositiveInteger (val1, val2) {
+  const lower = Math.ceil(Math.min(Math.abs(val1), Math.abs(val2)));
+  const upper = Math.floor(Math.max(Math.abs(val1), Math.abs(val2)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-getRandomInt(0, 1);
+getRandomPositiveInteger(0, 1);
 
-function getRandomFloorInt(val1, val2, lenght) {
-  let max = (val2 >= val1) ? val2 : val1;
-  let min = (val2 >= val1) ? val1 : val2;
-
-  min = Math.ceil(min * 10 ** lenght);
-  max = Math.floor(max * 10 ** lenght);
-  return (Math.floor(Math.random() * (max - min + 1)) + min) / 10 ** lenght;
+function getRandomPositiveFloat (val1, val2, digits = 1) {
+  const lower = Math.min(Math.abs(val1), Math.abs(val2));
+  const upper = Math.max(Math.abs(val1), Math.abs(val2));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
 }
 
-getRandomFloorInt(1.1, 2.1, 10);
+getRandomPositiveFloat(1.1, 2.1, 10);
