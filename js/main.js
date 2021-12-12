8f403a85ad3b0b0e2096710a7a8b5b95e41ed6e8
qostua <qostua@gmail.com>
2021-12-12 21:16:58 +0300

    Добавил исходные данные для генерации

--- js/main.js
+++ js/main.js
@@ -11,3 +11,39 @@ function getRandomPositiveFloat (val1, val2, digits = 1) {
   return result.toFixed(digits);
 }
 
+const ANNOUNCEMENT_TYPES = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Гостиница',
+};
+const ANNOUNCEMENT_CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const ANNOUNCEMENT_CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const ANNOUNCEMENT_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const ANNOUNCEMENT_DESCRIPTION = [
+  'просторными',
+  'светлыми',
+  'удобными',
+  'оснащенными всем необходимым',
+];
+const ANNOUNCEMENT_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
