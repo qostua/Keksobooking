4157c2f9ca5f5784fe8c5b7062cdddeb57112076
qostua <qostua@gmail.com>
2021-12-14 19:14:10 +0300

    Добавил в модуль с утилитарными функциями ф-ию склонения существительных

--- js/utils.js
+++ js/utils.js
@@ -10,5 +10,9 @@ function getRandomPositiveFloat (val1, val2, digits = 1) {
   const result = Math.random() * (upper - lower) + lower;
   return result.toFixed(digits);
 }
+function getWordCompletion(number, wordCompletions) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return wordCompletions[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+}
 
-export {getRandomPositiveInteger, getRandomPositiveFloat};
+export {getRandomPositiveInteger, getRandomPositiveFloat, getWordCompletion};
